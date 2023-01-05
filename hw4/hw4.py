20d9e4e032fd74549d8d9379a01e18d04ed0228f
Sergey Vorotilin <sflash@mail.ru>
2023-01-05 21:14:12 +0300

    hw4 доделанное

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -7,64 +7,158 @@
 
 # НЕОБЯЗАТЕЛЬНОЕ, ДОПОЛНИТЕЛЬНОЕ ЗАДАНИЕ:
 # Расширить значение коэффициентов до [-100..100]
-import random as rnd
-import math
 
-def multy_assembler(count):
+import random as rnd
+
+# Создаем коэффицинты многочлена
+def create_dic(count):
+    if count <0:
+        return -1
     i=0
     k=count
     koef={}
-    multi_string=''
-    while i<=k:
-        koef[i]=rnd.randint(-100,100)
-        
-        # Первый коэффициент не равен 0
-        
-        if i==2:
-            koef[i]=1
-        if i==3:
-            koef[i]=-1
-        if i==4:
-            koef[i]=0
-                
-
-
-        print (f'i={i} k={koef[i]} ',end=' ')
-        if koef[i]==0:            
-            if i==0:
-                continue
-            else:
-                i+=1
-                continue
-        # Формируем x со степенью, учитываем степени 0 и 1
-        if i==0:
-            temp_pref=''
-        elif i==1:
-            temp_pref='x'
-        else:
-            temp_pref='x'+str(i)
+    for i in range(count+1):
+        koef[i]=koef[i]=rnd.randint(-100,100)
     
+    # Добавлены специально 0,1 и -1 для чистоты экспремента
+    koef[2]=0
+    koef[3]=1
+    koef[4]=-1
+    
+    return koef    
 
-        if koef[i]>1:
-            temp_pref='+'+str(koef[i])+'*'+temp_pref
-        elif koef[i]<-1:
-            temp_pref=str(koef[i])+'*'+temp_pref
-        elif koef[i]==-1:
-            temp_pref='-'+temp_pref
-        elif koef[i]==1:
-            temp_pref='+'+temp_pref
-        if i==0:
-            if koef[i]==0:
-                temp_pref=''
-            elif koef[i]>0:
-                temp_pref='+'+str(koef[i])
+# Принимаем строку и возвращаем словарь
+def multy_desassembler(multy_str):
+    d_koef = {}
+    temp_list = str(multy_str).replace('-', '+-').replace('*', '').split('+')
+    max = 0
+    for i in range(len(temp_list)):
+        temp_list[i] = temp_list[i].split('x')
+
+        if (len(temp_list[i]) == 1):
+            my_key = 0
+            my_value = int(temp_list[i][0])
+        else:
+            if temp_list[i][0] == '':
+                temp_list[i][0] = '1'
+            if temp_list[i][1] == '':
+                temp_list[i][1] = '1'
+            if temp_list[i][0] == '-':
+                temp_list[i][0] = '-1'
+            my_key = int(temp_list[i][1])
+            my_value = int(temp_list[i][0])
+        d_koef[int(my_key)] = int(my_value)
+        if my_key > max:
+            max = my_key
+    if (len(d_koef) < max+1):
+
+        # Заполняем пустые ключи нулями
+        for i in range(max+1):
+            # print(d_koef.get(i))
+            if (d_koef.get(i) == None):
+                d_koef[i] = 0
+
+    return d_koef
+
+#Отправляем словарь с коэффицинтами, где:
+# ключ - чтепень при х
+# значение - коэффициент
+def multy_assembler(koefs):
+    i = 0
+    k = len(koefs)
+    koef = koefs
+    multi_string = ''
+    while i < k:
+
+        # Первый коэффициент не равен 0
+        if koef[i] == 0:
+            if i == 0:
+                continue
             else:
-                temp_pref=str(koef[i])
-        i+=1
-        print (temp_pref)
-        multi_string=temp_pref+multi_string
-    print(multi_string)
-    return  multi_string[1:]
+                i += 1
+                continue
 
-k=int(input('Введите степень многочлена '))
-print (multy_assembler(k))
+        # Формируем x со степенью, учитываем степени 0 и 1
+        if i == 0:
+            temp_pref = ''
+        elif i == 1:
+            temp_pref = 'x'
+        else:
+            temp_pref = 'x'+str(i)
+
+        # Формируем коэффициенты при x учитывая знак и значения 1,-1 и 0
+        if koef[i] > 1:
+            temp_pref = '+'+str(koef[i])+'*'+temp_pref
+        elif koef[i] < -1:
+            temp_pref = str(koef[i])+'*'+temp_pref
+        elif koef[i] == -1:
+            temp_pref = '-'+temp_pref
+        elif koef[i] == 1:
+            temp_pref = '+'+temp_pref
+
+        # Отдельно проверяем свободный член
+        if i == 0:
+            if koef[i] == 0:
+                temp_pref = ''
+            elif koef[i] > 0:
+                temp_pref = '+'+str(koef[i])
+            else:
+                temp_pref = str(koef[i])
+        i += 1
+        multi_string = temp_pref+multi_string
+
+    return multi_string[1:]
+
+#Принимаем две строки с многочленами и возвращам их сумму
+def sum(multy1, multy2):
+
+    koef1 = multy_desassembler(multy1)
+    koef2 = multy_desassembler(multy2)
+
+    if (len(koef1) > len(koef2)):
+        max_len = len(koef1)
+    else:
+        max_len = len(koef2)
+    koef3 = {}
+    for i in range(max_len):
+        koef3[i] = koef1.get(i, 0)+koef2.get(i, 0)
+
+    return multy_assembler(koef3)
+
+# Создаем два многочлена и сохраняем их в разные файлы (по условию)
+
+str1 = multy_assembler(create_dic(int(input ('Введите степень первого многочлна '))))
+
+my_file = open('file1.txt','w')
+my_file.writelines(str1)
+my_file.close
+
+str1 = multy_assembler(create_dic(int(input ('Введите степень второго многочлна '))))
+
+my_file = open('file2.txt','w')
+my_file.writelines(str1)
+my_file.close
+
+# читываем многочлены из файлов в разные строки (по условию задачи)
+my_file = open('file1.txt','r')
+str1=my_file.readline()
+my_file.close
+print ('Многочлен 1')
+print(str1)
+
+my_file = open('file2.txt','r')
+str2=my_file.readline()
+my_file.close
+print ('Многочлен 2')
+print(str2)
+print ('Сумма многочленов')
+str3=sum(str1, str2)
+print(str3)
+
+my_file = open('file3.txt','w')
+my_file.writelines(str3)
+my_file.close
+
+
+
+
